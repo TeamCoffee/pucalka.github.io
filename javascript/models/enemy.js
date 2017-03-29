a497b4fdef21f60f6c61c65382e64c8cccfc8c43
petkoche <petkoche90@gmail.com>
2017-03-29 03:08:53 +0300

    Add files via upload
    Enemy class, simple direction with linear exponential speed up.

---
+++ javascript/models/enemy.js
@@ -0,0 +1,85 @@
+var counter=0;
+class enemy extends gameObject {
+    constructor(pos, size, enemyImgAsSprite, speed, spriteTimerSpeed) {
+        super(pos, size);
+        this.enemyImgAsSprite = enemyImgAsSprite;
+        this.speed = speed;
+        this.spriteTimer = 0;
+        this.spriteTimerSpeed = spriteTimerSpeed;        
+    }
+    
+    get spriteTimer() {
+        return this._spriteTimer;
+    }
+
+    set spriteTimer(value) {
+        if (!(typeof(value) == "number")) {
+            throw new Error("Passed argument should be number");
+        }
+
+        this._spriteTimer = value;
+    }
+
+    get spriteTimerSpeed() {
+        return this._spriteTimerSpeed;
+    }
+
+    set spriteTimerSpeed(value) {
+        if (!(typeof(value) == "number")) {
+            throw new Error("Passed argument should be number");
+        }
+
+        this._spriteTimerSpeed = value;
+    }
+
+    get enemyImgAsSprite() {
+        return this._enemyImgAsSprite;
+    }
+
+    set enemyImgAsSprite(value) {
+        if (!(value instanceof spritesheet)) {
+            throw new Error('enemy image must be a spritesheet');
+        }
+
+        this._enemyImgAsSprite = value;
+    }
+
+    get speed() {
+        return this._speed;
+    }
+
+    set speed(value) {
+        if (!(value instanceof vector)) {
+            throw new Error('enemy Speed must be a vector');
+        }
+
+        this._speed = value;
+    }
+
+    enemyUpdatePosition() {
+        counter++;
+        this.pos.x -= (counter/5)*this.speed.x;        
+        console.log(this.pos.x);
+       
+    }
+    
+    update() {
+        this.enemyUpdatePosition();
+        
+        this.spriteTimer += this.spriteTimerSpeed;
+    }
+
+    calculateSpriteN() {
+        var spriteN = Math.floor(this.spriteTimer) %
+            this.enemyImgAsSprite.allSpritesCount();
+        return spriteN;
+    }
+
+    enemyDraw(context) {
+        var spriteN = this.calculateSpriteN();
+        this.enemyImgAsSprite.drawSprite(this.pos,
+            this.size,
+            spriteN,
+            context);
+    }
+}
