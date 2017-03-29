03f56c96c84192581560148ebe9dfb4f70d8930c
starter_e <ivanovemilplamenov@gmail.com>
2017-03-29 13:34:05 +0300

    Add files via upload
    Holds all enemies adds, removes and performs checks

--- javascript/holders/enemyHolder.js
+++ javascript/holders/enemyHolder.js
@@ -1,50 +1,94 @@
-//Depends on enemy
-//Depends on vector
-//it holds all enemy as an array and give a easy way to update and draw them simultaniously on the canvas
-class enemyHolder {
+//Depends on gameObject
+//it holds all enemies 
+
+class EnemyHolder extends gameObject {
+
     constructor() {
-        this.enemy = new Array();
+        this.totalEnemies = new Array();
     }
 
-    get enemy() {
-        return this._enemy;
+    get totalEnemies() {
+        return this.totalEnemies;
     }
 
-    set enemy(value) {
+    set totalEnemies(value) {
         if (!(value instanceof Array)) {
-            throw new Error("Passed argument should be Array");
+            throw new Error("Passed argument should be an array of enemies");
         }
-        this._enemy = value;
+        this.totalEnemies = value;
     }
 
-    addenemy(added) {
-        if (!(added instanceof enemy)) {
-            throw new Error("Passed arguments should be of type enemy");
+    add(enemy) {
+        if (enemy === undefined || !(enemy instanceof Enemy)) {
+            throw new Error('Argument should be an enemy to add.');
         }
 
-        this.enemy.push(added);
+        totalEnemies.push(enemy);
     }
 
-    removeenemy(arrayIndex) {
-        var enemysCount = this.enemy.length;
-        var lastArrayenemy = this.enemy[enemysCount];
+    isEnemyOutsideField(enemy) {
+        var enemyIsOutsideField = (enemy.pos.x > canvas.width
+                                    ||
+                                    enemy.pos.y > canvas.height
+                                    ||
+                                    enemy.pos.x < 0
+                                    ||
+                                    enemy.pos.y < 0)
 
-        this.enemy[arrayIndex] = lastArrayenemy;
-        this.enemy.pop();
+        return enemyIsOutsideField;
+    }
+
+    remove(enemyId) {
+        if (totalEnemies !== null) {
+
+            var matchingEnemy = totalEnemies.find(e => e.id === enemyId);
+
+            if (isEnemyOutsideField(matchingEnemy)){
+                var index = totalEnemies.indexOf(matchingEnemy);
+                totalEnemies.splice(index, 1);
+            }
+            else {
+                for (var i = 0; i < totalEnemies.length; i++) {
+                    if (enemyId === totalEnemies[i].id) {
+                        var index = totalEnemies.indexOf(totalEnemies[i]);
+                        totalEnemies.splice(index, 1);
+                    }
+                }
+            }
+        }
+
     }
 
     update() {
         var enemysCount = this.enemy.length;
         for (var i = 0; i < enemysCount; i++) {
-            this.enemy.update();
+            if (!(isEnemyOutsideField(totalEnemies[i]))) {
+                totalEnemies[i].update();
+            }
+            
         }
     }
-
+    
     draw(context) {
         var enemysCount = this.enemy.length;
         for (var i = 0; i < enemysCount; i++) {
-            this.enemy.draw(context);
+            if (!(isEnemyOutsideField(totalEnemies[i]))) {
+                totalEnemies[i].draw(context);
+            }
         }
     }
 
+    collidesWith(gameObject) {
+        for (var i = 0; i < totalEnemies.length; i++) {
+            if (totalEnemies[i].pos.x === gameObject.pos.x
+                &&
+                totalEnemies[i].pos.y === gameObject.pos.y) {
+                return totalEnemies[i].id;
+            }
+            else {
+                return null;
+            }
+        }
+    }
 }
+
